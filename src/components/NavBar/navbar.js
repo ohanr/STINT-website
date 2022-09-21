6d40cd2184e691c87c0b7ae0ebc105465c50166a
RuiYiYong <yongruiyi@hotmail.com>
2022-09-21 17:43:05 +1000

    Navbar Final + Home Page Update
    Navbar updated to resize nicely for different screen sizes and align logo. Added homepage resizing dynamically.

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -3,48 +3,51 @@ import React, { useState } from "react";
 export default function Navbar({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return(
-        <nav class="bg-light_blue px-2 sm:px-4 py-2.5">
-        <div class="container flex flex-wrap justify-between items-center mx-auto">
-          <a href="https://stintcommunity.com/" class="flex items-center">
-              <img src={require('./../Images/Logo2.png')} class="mr-0 w-72"/>
-          </a>
-          <button data-collapse-toggle="navbar-default" onClick={() => setNavbarOpen(!navbarOpen)} type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-          </button>
-          <div class="hidden w-full md:block md:w-auto" id="navbar-default" className={
-              "lg:flex flex-grow items-right" +
-              (navbarOpen ? " flex" : " hidden")
-            }
-          >
-            <ul class="text-left flex flex-col md: pt-4 lg:p-4 mt-4 bg-transparent-50 rounded-lg border md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900">
-              <li>
-                <button class="sm:py-6 pr-5 xl:ml-64 bg-transparent text-gray-700 rounded font-bold md:py-2 px-4 rounded">
-                  <a href="#" class="lexend text-base block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-mid_blue md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                    Job Board
-                  </a>
-                </button>
-              </li>
-                <button class="lg:pr-2 pr-10 bg-transparent text-gray-700 rounded font-bold md:py-2 rounded">
-                  <a href="#" class="lexend text-base block py-2 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-mid_blue md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                    For Companies
-                  </a>
-                </button>
-              <li>
-                <button class="pr-4 bg-transparent text-gray-700 rounded font-bold md:py-2 px-4 rounded">
-                  <a href="#" class="lexend text-base block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-mid_blue md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                    About Us
-                  </a>
-                </button>
-              </li>
-              <li>
-                <button class="bg-dark_blue text-white font-bold md:py-1 md:my-1 my-3 ml-5 px-3 rounded-lg md:hover:bg-dark_blue">
-                  <a href="https://docs.google.com/forms/d/e/1FAIpQLScEg-PTGW04bk_ctv62nwj0t2gj_ZBI7BVQj82L4lQtb5AI2g/viewform" class="lexend text-base block py-2 pr-4 pl-3 text-white bg-transparent rounded md:bg-transparent md:text-white md:p-0 dark:text-white">
-                    Join our Community
-                  </a>
-                </button>
-              </li>
-            </ul>
+        <nav class="static bg-light_blue lg:py-5 py-2.5 w-full">
+        <div class="static container mx-auto flex flex-wrap justify-between lg:pl-0 lg:mr-12 w-full items-center">
+          <div class = "float-left w-3/12">
+            <a href="https://stintcommunity.com/" class="md:absolute left-0 top-0 pt-5 pl-5 md:pl-14 flex items-center">
+                <img src={require('./../Images/Logo2.png')} class="mr-0 lg:w-80 w-52"/>
+            </a>
+          </div>
+          <div class = "float-right mr-5">
+            <button data-collapse-toggle="navbar-default" onClick={() => setNavbarOpen(!navbarOpen)} type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+              <span class="sr-only">Open main menu</span>
+              <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            </button>
+            <div class="hidden w-full md:block md:w-auto pt-5 " id="navbar-default" className={
+                "md:flex flex-grow items-right" +
+                (navbarOpen ? " flex" : " hidden")
+              }>
+              <ul class="text-left flex flex-col md:p-0 mt-4 bg-transparent-50 rounded-lg border md:flex-row lg:space-x-10 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900">
+                <li>
+                  <button class="sm:py-6 lg:pr-6 xl:ml-72 ml-0 bg-transparent text-gray-700 rounded font-bold md:py-2 px-4 rounded">
+                    <a href="#" class="lexend text-base block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-mid_blue md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                      Job Board
+                    </a>
+                  </button>
+                </li>
+                  <button class="md:pr-2 bg-transparent text-gray-700 rounded font-bold md:py-2 rounded">
+                    <a href="#" class="lexend text-base block py-2 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-mid_blue md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                      For Companies
+                    </a>
+                  </button>
+                <li>
+                  <button class="pr-4 bg-transparent text-gray-700 rounded font-bold md:py-2 px-4 rounded">
+                    <a href="#" class="lexend text-base block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-mid_blue md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                      About Us
+                    </a>
+                  </button>
+                </li>
+                <li>
+                  <button class="bg-dark_blue text-white font-bold md:py-1 md:my-1 my-3 ml-5 px-3 rounded-lg md:hover:bg-dark_blue">
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLScEg-PTGW04bk_ctv62nwj0t2gj_ZBI7BVQj82L4lQtb5AI2g/viewform" class="lexend text-base block py-2 text-white bg-transparent rounded md:bg-transparent md:text-white md:p-0 dark:text-white">
+                      Join our Community
+                    </a>
+                  </button>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -7,103 +7,64 @@ const HomePage = () => {
         @import url('https://fonts.googleapis.com/css2?family=Lexend&display=swap');
       </style>
       <Navbar></Navbar>
-      <div class = "bg-light_blue pt-12 pb-32 w-full min-h-[600px]">
-        <div class = "bg-transparent w-full md:w-5/12 float-center md:float-right">
-          <div className="ml-24 md:mt-12 rounded-full bg-circle_blue object-center w-5/12 md:w-7/12">
-            <img class = "mx-auto object-center" src={require('./../Images/Hats.png')}/>
+      <div class = "bg-light_blue pt-10 pb-32 w-full min-h-[200px]">
+        <div class = "bg-transparent pb-5 w-full lg:w-5/12 float-center lg:float-right">
+          <div className="mx-auto lg:mt-4 rounded-full bg-circle_blue object-center w-5/12 lg:w-7/12">
+            <img class = "object-center w-auto" src={require('./../Images/Hats.png')}/>
           </div>
         </div>
-        <div class = "bg-transparent w-full md:w-7/12 float-left pl-20">
-          <p class = "text-left lexend text-4xl md:text-6xl font-bold">
-            Largest 
+        <div class = "bg-transparent w-full lg:w-7/12 px-12 lg:px-0 lg:pl-20">
+          <p class = "lg:text-left lexend text-center text-4xl lg:w-9/12 lg:text-6xl lg:leading-none font-bold">
+            Largest International Student Community in Australia
           </p>
-          <p class = "text-left lexend  text-4xl md:text-6xl font-bold pt-3">
-            International
-          </p>
-          <p class = "text-left lexend  text-4xl md:text-6xl font-bold pt-3">
-            Student Community
-          </p>
-          <p class = "text-left lexend  text-4xl md:text-6xl font-bold pt-3">
-            in Australia
-          </p>
-          <p class = "text-left lexend pt-5 text-sm md:text-lg mr-24">
+          <p class = "lg:text-left lexend pt-5 text-sm lg:text-lg lg:mr-24">
           Gain personalised support from other international students in Australia and secure your dream job now!
           </p>
-          <div class = "mr-36">
-            <button class="bg-dark_blue text-white w-full font-bold mt-8 py-3 rounded-lg md:hover:bg-dark_blue">
+          <div class = "lg:mr-36">
+            <button class="bg-dark_blue text-white w-full font-bold mt-8 py-3 rounded-lg lg:hover:bg-dark_blue">
               <a href="https://docs.google.com/forms/d/e/1FAIpQLScEg-PTGW04bk_ctv62nwj0t2gj_ZBI7BVQj82L4lQtb5AI2g/viewform" class="text-xs lg:text-xl lexend text-base block py-2 text-white bg-transparent rounded md:bg-transparent md:text-white md:p-0 dark:text-white">
                   Join Our Community!
               </a>
             </button>
-              <div class = "pt-5 float-left w-5/12 lg:w-6/12 pr-2">
-                <button class="float-left bg-mid_blue w-full text-white font-bold py-3 rounded-lg md:hover:bg-dark_blue">
-                  <a href="https://docs.google.com/forms/d/e/1FAIpQLScEg-PTGW04bk_ctv62nwj0t2gj_ZBI7BVQj82L4lQtb5AI2g/viewform" class="text-xs lg:text-xl lexend text-base block py-2 text-white bg-transparent rounded md:bg-transparent md:text-white md:p-0 dark:text-white">
-                      Job Board
-                  </a>
-                </button>
-              </div>
-              <div class = "pt-5 float-right w-7/12 lg:w-6/12 pl-2">
-                <button class="float-right bg-mid_blue w-full text-white font-bold py-3 rounded-lg md:hover:bg-dark_blue">
-                  <a href="https://docs.google.com/forms/d/e/1FAIpQLScEg-PTGW04bk_ctv62nwj0t2gj_ZBI7BVQj82L4lQtb5AI2g/viewform" class="text-xs lg:text-xl lexend text-base block py-2 text-white bg-transparent rounded md:bg-transparent md:text-white md:p-0 dark:text-white">
-                    For Companies
-                  </a>
-                </button>
-              </div>
-            </div>
-          </div>
-        {/* <div class = "w-7/12">
-          <p class = "text-left lexend text-4xl md:text-6xl font-bold">
-            Largest 
-          </p>
-          <p class = "text-left lexend  text-4xl md:text-6xl font-bold pt-3">
-            International
-          </p>
-          <p class = "text-left lexend  text-4xl md:text-6xl font-bold pt-3">
-            Student Community
-          </p>
-          <p class = "text-left lexend  text-4xl md:text-6xl font-bold pt-3">
-            in Australia
-          </p>
-          <p class = "text-left lexend pt-5 text-sm md:text-xl mr-10">
-            Gain personalised support from other international students and access our exclusive Job Board.
-          </p>
-          <div class = "pt-5 float-center mr-10">
-            <button class="bg-dark_blue text-white w-full font-bold py-3 rounded-lg md:hover:bg-dark_blue">
-              <a href="https://docs.google.com/forms/d/e/1FAIpQLScEg-PTGW04bk_ctv62nwj0t2gj_ZBI7BVQj82L4lQtb5AI2g/viewform" class="text-xs lg:text-xl lexend text-base block py-2 text-white bg-transparent rounded md:bg-transparent md:text-white md:p-0 dark:text-white">
-                  Join Our Community!
-              </a>
-            </button>
-            <div class = "pt-5 float-left w-5/12 lg:w-6/12">
-            <button class="float-left bg-mid_blue w-11/12 text-white font-bold py-3 rounded-lg md:hover:bg-dark_blue">
+            <div class = "pt-5 float-left w-6/12 pr-2">
+              <button class="float-left bg-mid_blue w-full text-white font-bold py-3 rounded-lg lg:hover:bg-dark_blue">
                 <a href="https://docs.google.com/forms/d/e/1FAIpQLScEg-PTGW04bk_ctv62nwj0t2gj_ZBI7BVQj82L4lQtb5AI2g/viewform" class="text-xs lg:text-xl lexend text-base block py-2 text-white bg-transparent rounded md:bg-transparent md:text-white md:p-0 dark:text-white">
                     Job Board
                 </a>
               </button>
             </div>
-            <div class = "pt-5 float-right w-7/12 lg:w-6/12">
-              <button class="float-right bg-mid_blue w-11/12 text-white font-bold py-3 rounded-lg md:hover:bg-dark_blue">
+            <div class = "pt-5 float-right w-6/12 pl-2">
+              <button class="float-right bg-mid_blue w-full text-white font-bold py-3 rounded-lg lg:hover:bg-dark_blue">
                 <a href="https://docs.google.com/forms/d/e/1FAIpQLScEg-PTGW04bk_ctv62nwj0t2gj_ZBI7BVQj82L4lQtb5AI2g/viewform" class="text-xs lg:text-xl lexend text-base block py-2 text-white bg-transparent rounded md:bg-transparent md:text-white md:p-0 dark:text-white">
                   For Companies
                 </a>
               </button>
             </div>
-            <div class = "pt-5 float-left">
-              <span class = "lexend text-left text-xl">
-                Join over 400+ international students in Australia and secure your dream job now!
-              </span>
+          </div>
+        </div>
+      </div>
+      <div class = "pt-10 bg-white min-h-[1000px]">
+        <p class = "text-4xl lexend font-bold">Why Join Stint?</p>
+        <p class = "pt-5 pb-10 text-lg lexend">Here at Stint, we are building a more diverse and inclusive Australia workforce.</p>
+        <div class = "w-full bg-tranparent float-left md:w-6/12">
+          <div class = "py-5">
+            <div class = "w-2/12 float-left">
+              <img class = "ml-12 object-center py-3" src={require('./../Images/Resource Logo.png')}/>
+              <img class = "ml-12 object-center py-3" src={require('./../Images/Resource Logo.png')}/>
+              <img class = "ml-12 object-center py-3" src={require('./../Images/Resource Logo.png')}/>
+              <img class = "ml-12 object-center py-3" src={require('./../Images/Resource Logo.png')}/>
+            </div>
+            <div class = "w-9/12 float-right">
+              <p class = "py-3 lexend font-bold text-lg text-left">
+                Resources
+              </p>
+              <p class = "text-left text-sm lexend mr-10 pt-5">
+                Gain access to weekly, helpful resources to inch you closer to landing a professional career in Australia. We offer our community benefits such as FREE CV reviews!
+              </p>
             </div>
           </div>
         </div>
-        <div class = "xl:pt-12 w-3/12 float-right">
-          <div className="xl:p-0 rounded-full bg-circle_blue object-center">
-            <img class = "mx-auto object-center" src={require('./../Images/Hats.png')}/>
-          </div>
-    </div>*/}
-      </div>
-      <div class = "pt-10 bg-white min-h-[500px]">
-        <p class = "text-4xl lexend font-bold">Why Join Stint?</p>
-        <p class = "pt-5 pb-10 text-md lexend">Here at Stint, we are building a more diverse and inclusive Australia workforce.</p>
-        <div class = "w-full bg-black md:w-6/12">Hi</div>
+        <div class = "w-full bg-black float-right md:w-6/12">Hi</div>
       </div>
       <div class = "bg-light_blue min-h-[500px]">HI BLUE</div>
     </div>
