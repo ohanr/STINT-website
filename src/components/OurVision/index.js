e7c032f3f2e966abc21c4b79b5982db004401631
ohanr <ohan.ra@arup.com>
2022-09-21 18:04:17 +1000

    Our vision

--- src/components/OurVision/index.js
+++ src/components/OurVision/index.js
@@ -2,15 +2,14 @@ import styled from "styled-components";
 
 //position absolute -> can overlay
 const Container = styled.div`
-    display: flex;
-    padding: 20px;
-    width: 100%;
+    width: initial;
     justify-content: space-between;
     position: relative
 `;
 
 const Title = styled.h1`
     text-align: justify;
+    margin-left: 20px;
     font-weight: bold;
     font-size: 30px;
     color: ${(props) => props.color};
@@ -19,19 +18,19 @@ const Title = styled.h1`
 
 const Text = styled.p`
     text-align: justify;
-    margin: 20px 0px;
-    width: 70%;
+    margin-left: 20px;
+    width: 100%;
     colour: ${(props) => props.color};
     z-index: 2;
 `
 
 const OurVisionContainer = styled.div`
-    width: 100%
+    width: 70%
 `;
 
 const Border = styled.div`
     background-color: #1F628C;
-    width: 100wh;
+    width: 100%;
     height: 10vh;
     z-index: 1;
 `;
@@ -42,19 +41,20 @@ const OurVision = () => {
             <OurVisionContainer>
                 <Title color="black">Our Vision</Title>
                 <Text>
-                Here at Stint, we firmly believe that anyone, regardless of citizenship status, deserves a fair chance at a future working in Australia.
+                    Here at Stint, we firmly believe that anyone, regardless of citizenship status, deserves a fair chance at a future working in Australia.
 
-                Ultimately, we aim to create a more equitable job landscape - supporting international students to land relevant jobs and internships, and educating employers to judge students based on their skills and experiences, rather than their citzenship status.
+                    Ultimately, we aim to create a more equitable job landscape - supporting international students to land relevant jobs and internships, and educating employers to judge students based on their skills and experiences, rather than their citzenship status.
 
-                That’s why our vision is to deliver a better future for international students in Australia by transforming the way they are viewed - by employers, universities and governments alike.
-                </Text>
-                <Border>
-                   <Title color="white">
-                        Our Story So Far...
-                    </Title> 
-                </Border>
-                
+                    That’s why our vision is to deliver a better future for international students in Australia by transforming the way they are viewed - by employers, universities and governments alike.
+                </Text> 
             </OurVisionContainer>
+            <Border>
+                <Title color="white">
+                    Our Story So Far...
+                </Title> 
+            </Border>
+            
+           
         </Container>
     )
 }
